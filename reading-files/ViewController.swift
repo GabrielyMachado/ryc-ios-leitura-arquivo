26f3142be30cbbebe7d1f46bc78c009b031355f0
Gabriely Machado <gabriely.a.machado@accenture.com>
2025-03-20 20:58:43 -0300

    Initial Commit

---
+++ reading-files/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reading-files
+//
+//  Created by gabriely.a.machado on 20/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reading-filesTests/reading_filesTests.swift
@@ -0,0 +1,17 @@
+//
+//  reading_filesTests.swift
+//  reading-filesTests
+//
+//  Created by gabriely.a.machado on 20/03/25.
+//
+
+import Testing
+@testable import reading_files
+
+struct reading_filesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
